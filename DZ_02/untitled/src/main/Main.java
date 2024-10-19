68aa3b8d9ecacef422acac1a57192a3cd8a1a851
Артем <@DESKTOP-LVIIU93>
2024-10-19 17:31:59 +0300

    DZ_02(2)

---
+++ DZ_02/untitled/src/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Your time is limited,\n\tso don`t waste it\n\t\tliving someone else`s life\n\t\t\t-Steve Jobs");
+    }
+}
